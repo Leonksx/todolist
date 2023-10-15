8720820a825a051f99c154db650e4c6c18d7b159
Leonardo Serra Xavier <leon_ksx@hotmail.com>
2023-10-15 13:40:00 -0300

    Dockerfile deploy

---
+++ src/main/java/br/com/leonardoserraxavier/todolist/errors/ExceptionHandlerController.java
@@ -0,0 +1,17 @@
+package br.com.leonardoserraxavier.todolist.errors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionHandlerController {
+
+  // Gerando o erro no title que pode ter no máximo 50 caracteres
+  @ExceptionHandler(HttpMessageNotReadableException.class)
+  public ResponseEntity<String> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMostSpecificCause().getMessage());
+  }
+}
